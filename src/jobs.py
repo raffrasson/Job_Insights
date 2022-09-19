053461b35da691a62201a7f001e35eb7acb1007f
raffrasson <raf.frasson@gmail.com>
2022-09-19 20:42:03 -0300

    feat: completes req. 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,14 @@
 from functools import lru_cache
+import csv
+
+
 
 
 @lru_cache
 def read(path):
+    with open(path, encoding = "utf-8") as file:
+      reader = csv.DictReader(file, delimiter=",", quotechar='"')
+      return list(reader)
     """Reads a file from a given path and returns its contents
 
     Parameters
