a6912425ce3aa6926faf8f2b0a0f3d420e5441b7
raffrasson <raf.frasson@gmail.com>
2022-09-19 21:24:30 -0300

    fix: formatting

--- src/jobs.py
+++ src/jobs.py
@@ -2,23 +2,8 @@ from functools import lru_cache
 import csv
 
 
-
-
 @lru_cache
 def read(path):
-    with open(path, encoding = "utf-8") as file:
-      reader = csv.DictReader(file, delimiter=",", quotechar='"')
-      return list(reader)
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, encoding="utf-8") as file:
+        reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        return list(reader)
